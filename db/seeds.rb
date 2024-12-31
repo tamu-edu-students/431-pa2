90e205b903010896d63016c44f742ef40e1e5db2
Vivek Somarapu <vivek.somarapu@gmail.com>
2024-12-31 02:18:21 +0000

    seeding 3 more movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,22 @@
 
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
-  {:title => 'My Neighbor Totoro', :rating => 'G',
-    :release_date => '16-Apr-1988'},
-  {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
-  {:title => 'Parasite', :rating => 'R',
-    :release_date => '30-May-2019'},
-  {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
-  {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+#   {:title => 'My Neighbor Totoro', :rating => 'G',
+#     :release_date => '16-Apr-1988'},
+#   {:title => 'Green Book', :rating => 'PG-13',
+#     :release_date => '16-Nov-2018'},
+#   {:title => 'Parasite', :rating => 'R',
+#     :release_date => '30-May-2019'},
+#   {:title => 'Nomadland', :rating => 'R',
+#     :release_date => '19-Feb-2021'},
+#   {:title => 'CODA', :rating => 'PG-13',
+#     :release_date => '13-Aug-2021'}, 
+  {:title => 'The Prestige', :rating => 'PG-13',
+    :release_date => '20-Oct-2006'},
+  {:title => 'The Godfather', :rating => 'R',
+    :release_date => '24-Mar-1972'},
+  {:title => 'Old Boy', :rating => 'R',
+    :release_date => '21-Nov-2003'},
 ]
 
 more_movies.each do |movie|
